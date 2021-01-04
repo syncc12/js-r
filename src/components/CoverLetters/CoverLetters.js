b73eba0b3e584a58098944339e062591c084e9cd
Patrick Lyden <syncc12@verizon.net>
2021-01-04 18:18:18 +0000

    Updated Cover Letter Generation to allow for editing and copying/cutting and pasting prior to downloading as PDF (if you so choose).

--- src/components/CoverLetters/CoverLetters.js
+++ src/components/CoverLetters/CoverLetters.js
@@ -2,46 +2,54 @@ import React from 'react';
 import './CoverLetters.scss';
 import { GlobalContext } from '../../contexts/global-context';
 import { jsPDF } from "jspdf";
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class CoverLetters extends React.Component {
   static contextType = GlobalContext;
 
-  constructor() {
-    super();
-    this.state = {
-      pdfSRC: '',
-      clPDF: ''
-    };
-  }
 
-  coverLetterDownload = () => {
-    const { clPDF } = this.state;
-    const { jobTitle, company } = this.props;
-    clPDF.save(`Cover Letter ${company} - ${jobTitle}.pdf`);
-  }
-  
-
-  componentDidMount() {
-    const { coverLetterText, jobTitle, company } = this.props;
-    
+  convertToPDF = (inCoverLetterText) => {
     let thisClPDF = new jsPDF();
-    const clText = thisClPDF.setFontSize(12).splitTextToSize(coverLetterText,170);
-    thisClPDF.text(20,20,clText);
-    const thisPDFSRC = thisClPDF.output('datauristring',`${jobTitle} - ${company} cover letter.pdf`);
-
-    this.setState({pdfSRC: thisPDFSRC, clPDF: thisClPDF});
+    const clText = thisClPDF.setFontSize(12).splitTextToSize(inCoverLetterText,170);
+    return thisClPDF.text(20,20,clText);
   }
+
+  coverLetterDownload = (e) => {
+    const { jobTitle, company } = this.props;
+
+    const currentCLText = e.target.getElementsByTagName('textarea')[0].value;
+
+    const clPDF = this.convertToPDF(currentCLText);
+
+    clPDF.save(`Cover Letter ${company} - ${jobTitle}.pdf`);
+    e.preventDefault();
+  }
+
   
   render () {
-    const { pdfSRC } = this.state;
+    const { coverLetterText } = this.props;
 
     return (
       <>
         <div>
-          <div id="cover-letter-download" className="fa fa-download fa-3x" onClick={(() => this.coverLetterDownload())}></div>
-        </div>
-        <div id="generated-cover-letter">
-          <div id="pdf-frame"><iframe title="cover_letter_iframe" id="pdf-iframe" src={pdfSRC}></iframe></div>
+          <div id="cover_letter-frame">
+            <Form onSubmit={((e) => this.coverLetterDownload(e))}>
+              <Form.Group>
+                <Form.Row>
+                  <Col xs={12}>
+                    <Form.Control id="cover_letter-text" as="textarea" defaultValue={coverLetterText !== undefined ? coverLetterText : ''} rows={15} />
+                  </Col>
+                </Form.Row>
+              </Form.Group>
+              <Button variant="primary" type="submit" size="lg">
+                <FontAwesomeIcon icon={['fad','download']} />
+              </Button>
+            </Form>
+          </div>
+
         </div>
       </>
     );
